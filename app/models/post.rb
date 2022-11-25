c3465f1148d28dd38c0196fccd5993a7312f8919
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-25 18:56:59 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :summary, length: {maximum: 250}
+    validates :content, length:{minimum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :clickbait?
+
+    ALLOWED = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess /i
+    ]
+
+
+    def clickbait?
+        if ALLOWED.none? { |click| click.match title}
+            errors.add(:title, "invalid title")
+        end
+    end
 end
